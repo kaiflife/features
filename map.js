8ecc26271c4623edcbdfa76bc0cb830be9f6771f
Pavel <34074420+kaiflife@users.noreply.github.com>
2022-06-16 16:59:35 +0300

    Delete map.js

--- map.js
+++
@@ -1,3 +0,0 @@
-export const getLatitudeLongitude = (coordinates) => coordinates.map(({ lng, lat }) => [lat, lng]);
-
-export const getMidpoint = (firstPoint, secondPoint) => [(+firstPoint[0] + +secondPoint[0]) / 2, (+firstPoint[1] + +secondPoint[1]) / 2];

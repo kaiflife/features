733388bea42b68f78b2979610262595bc3523f79
Pavel <34074420+kaiflife@users.noreply.github.com>
2022-07-28 14:08:47 +0300

    Add files via upload

---
+++ updateBuildTime.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+const fs = require('fs');
+
+const sw_file = './src/forceUpdateCache.js';
+
+const buildDate = `console.log(${new Date().getTime()})`;
+
+fs.writeFile(sw_file, buildDate, 'utf8', (error) => {
+  if (error) {
+    return console.warn(error);
+  }
+  return console.warn('All right');
+});
+
+
+// in your package.json scripts
+// "build": "node ./updateBuildTime.js && node scripts/build.js && cat src/forceUpdateCache.js >> build/service-worker.js",

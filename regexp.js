b6acafebdf88fa85d86e62b4217ae7fa5f2592c3
Pavel <34074420+kaiflife@users.noreply.github.com>
2023-02-13 01:24:45 +0300

    Update regexp.js

--- regexp.js
+++ regexp.js
@@ -17,3 +17,9 @@ export const emailRegex = (string) => {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return !!string.toLowerCase().match(regex);
 };
+
+export function validateEmail(email) {
+  const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+  return regex.test(email);
+}

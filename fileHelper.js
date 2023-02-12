7c23bdc41fd49b390b30a4f4541384ddbb62893f
Pavel <34074420+kaiflife@users.noreply.github.com>
2023-02-13 01:22:32 +0300

    Update fileHelper.js

--- fileHelper.js
+++ fileHelper.js
@@ -38,3 +38,52 @@ export const openFileFolder = ({ id = 'inputFile', onChange }) => {
   document.body.append(fileInputEl);
   fileInputEl.click();
 };
+
+export function downloadBlobFileOnClick(data, fileName) {
+  const href = typeof data === 'string' ? data : window.URL.createObjectURL(data);
+  const link = document.createElement('a');
+  link.style.display = 'none';
+  document.body.appendChild(link);
+  link.setAttribute('href', href);
+  link.setAttribute('target', '_blank');
+  link.setAttribute('download', fileName);
+  link.click();
+  document.body.removeChild(link);
+}
+
+export const getFileExtensions = (
+  extensions = ['pdf', 'doc', 'docx'],
+) => extensions.map((extension) => `.${extension}`).join(', ');
+
+export const getUrlFromFile = (file) => {
+  if (!file) return;
+  if (file?.url) return file.url;
+
+  return window.URL.createObjectURL(file);
+};
+
+export function blobToFile(theBlob, fileName) {
+  // A Blob() is almost a File() - it's just missing the two properties below which we will add
+  theBlob.lastModifiedDate = new Date();
+  theBlob.name = fileName;
+  return theBlob;
+}
+
+export function removeFileFromFileList(index, input) {
+  const dt = new DataTransfer();
+  const { files } = input;
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (index !== i) dt.items.add(file);
+  }
+
+  input.files = dt.files;
+}
+
+export function removeAllInputFiles(input) {
+  const dt = new DataTransfer();
+
+  input.files = dt.files;
+}

08e17f796e3e5d936274aebc65b82227edd092b7
Pavel <34074420+kaiflife@users.noreply.github.com>
2024-03-22 15:33:04 +0300

    Update testLogin.js

--- endToEndTests/testLogin.js
+++ endToEndTests/testLogin.js
@@ -1,36 +1,28 @@
-import { bodyQuerySelector, setNativeValue } from './testHelpers'
+import { testIdSelect, asyncSetValue } from './testHelpers';
 
 export const testLoginCase = {
   failure: {
-    email: 'defusen2@gmail.com',
-    password: 'qwe'
+    email: 'qwe',
+    password: 'qwe',
   },
   success: {
-    email: 'defusen2@gmail.com',
-    password: 'qwe2'
-  }
-}
+    email: 'email@mail.com',
+    password: 'qwe2',
+  },
+};
 
-export const testLogin = async (testCaseName = 'failure') => {
-  console.log('testCaseName', testCaseName);
-  const emailInput = bodyQuerySelector('input[name="email"]')
+export const testLogin = async () => {
+  const emailInput = testIdSelect('username');
 
   if (!emailInput) {
-    alert('Нужно перейти на страницу логина')
-    return
+    alert('Нужно перейти на страницу логина');
+    return;
   }
 
-  const passwordInput = bodyQuerySelector('input[name="password"]')
-  const submitButton = bodyQuerySelector('button[type="submit"]')
+  const submitButton = testIdSelect('login-submit');
 
-  const { email, password } = window.testLoginCase[testCaseName]
+  await asyncSetValue('username');
+  await asyncSetValue('password');
 
-  await setNativeValue({ value: email, element: emailInput })
-  await setNativeValue({ value: password, element: passwordInput })
-
-  if (passwordInput.value === password && emailInput.value === email) {
-    submitButton.click()
-  } else {
-    alert('Неверные значения у инпутов')
-  }
-}
+  submitButton.click();
+};

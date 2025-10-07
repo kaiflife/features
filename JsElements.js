659b00985464a7cb8b75edbc5b9c4388bded1104
Pavel <34074420+kaiflife@users.noreply.github.com>
2025-10-07 14:53:39 +0300

    фикс CustomSelector

--- JsElements.js
+++ JsElements.js
@@ -73,7 +73,7 @@ class CustomSelector {
             }
         };
 
-        this.containerEl.append(this.selectorList)
+        this.containerEl.append(this.optionsContainerEl)
     }
 
     getElement() {
